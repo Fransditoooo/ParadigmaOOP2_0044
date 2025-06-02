af4b63b89dbe2a7ca26d43a175bd51dc3b052823
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-06-02 11:26:37 +0700

    implementasi class budi

--- ParadigmaOOP2/solveMultiwaris.cpp
+++ ParadigmaOOP2/solveMultiwaris.cpp
@@ -34,4 +34,15 @@ public:
 };
 
 
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur):
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
 
